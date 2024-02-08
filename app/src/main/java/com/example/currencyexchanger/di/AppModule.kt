4f7dfe2ecdf5b1799d2f77e3d8df373cc7cf040d
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-02-09 00:53:09 +0200

    Created AppModule class to define modules which will be used in application.

---
+++ app/src/main/java/com/example/currencyexchanger/di/AppModule.kt
@@ -0,0 +1,49 @@
+package com.example.currencyexchanger.di
+
+import com.example.currencyexchanger.data.CurrencyApi
+import com.example.currencyexchanger.main.DefaultMainRepository
+import com.example.currencyexchanger.main.MainRepository
+import com.example.currencyexchanger.util.DispatcherProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+
+private const val BASE_URL = "https://developers.paysera.com/tasks/api"
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideCurrencyApi() : CurrencyApi = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(CurrencyApi::class.java)
+
+    @Singleton
+    @Provides
+    fun provideMainRepository(api: CurrencyApi): MainRepository = DefaultMainRepository(api)
+
+    @Singleton
+    @Provides
+    fun provideDispatchers(): DispatcherProvider = object :DispatcherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+    }
+}
