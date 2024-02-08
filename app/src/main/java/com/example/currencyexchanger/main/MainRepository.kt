cd90042d10ff8751a371156f3624a8c6d69b6f12
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-02-09 00:57:41 +0200

    Created MainRepository interface to declare methods that will be work with datasource.

---
+++ app/src/main/java/com/example/currencyexchanger/main/MainRepository.kt
@@ -0,0 +1,9 @@
+package com.example.currencyexchanger.main
+
+import com.example.currencyexchanger.models.CurrencyResponse
+import com.example.currencyexchanger.util.Resource
+
+interface MainRepository {
+
+    suspend fun getRates(base: String) : Resource<CurrencyResponse>
+}
