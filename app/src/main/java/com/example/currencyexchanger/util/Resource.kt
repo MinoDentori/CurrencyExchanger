27bed2741cfe1562112d21a2e4f71472635d1f52
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-02-14 09:05:29 +0200

    Updated Resource class, added Empty class.

--- app/src/main/java/com/example/currencyexchanger/util/Resource.kt
+++ app/src/main/java/com/example/currencyexchanger/util/Resource.kt
@@ -1,6 +1,9 @@
 package com.example.currencyexchanger.util
 
+import com.example.currencyexchanger.models.CurrencyResponse
+
 sealed class Resource<T> (val data: T?, val message: String?) {
     class Success<T>(data: T) : Resource<T>(data, null)
     class Error<T> (message: String) : Resource<T>(null, message)
+    data object Empty : Resource<CurrencyResponse>(null, null)
 }
