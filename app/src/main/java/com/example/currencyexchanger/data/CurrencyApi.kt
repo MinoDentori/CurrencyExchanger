566d391b28c891458c8c7cbbbecbdac083ba08c5
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-02-14 09:00:08 +0200

    Updated API get currency request.

--- app/src/main/java/com/example/currencyexchanger/data/CurrencyApi.kt
+++ app/src/main/java/com/example/currencyexchanger/data/CurrencyApi.kt
@@ -3,12 +3,8 @@ package com.example.currencyexchanger.data
 import com.example.currencyexchanger.models.CurrencyResponse
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface CurrencyApi {
-
-    @GET("/currency-exchange-rates")
-    suspend fun getRates(
-        @Query("base") base: String
-    ): Response<CurrencyResponse>
+    @GET("currency-exchange-rates")
+    suspend fun getRates(): Response<CurrencyResponse>
 }
